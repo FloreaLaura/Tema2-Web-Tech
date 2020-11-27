5ff94e58d316b7fb6ab32c0c4f93382eb9525c73
FloreaLaura <florealaura18@stud.ase.ro>
2020-11-27 14:44:50 +0200

    Tema2

--- main/app.js
+++ main/app.js
@@ -1,5 +1,35 @@
 function addTokens(input, tokens){
-    
+     //a)
+     if (typeof input !== 'string')
+     {
+        throw("Invalid input");
+     }
+ 
+     //b)
+     if(input.length < 6){
+         throw 'Input should have at least 6 characters';
+     }
+
+     //c)
+     if (tokens === null) {
+        throw Error('Invalid array format');
+     }
+     else 
+     if (typeof tokens[0].tokenName !== typeof 'string' ) {
+        throw Error('Invalid array format');
+     }
+
+     //d
+     if(!(input.includes("...", 0))){
+        return input;
+    }
+
+    //e
+    if(input.includes('...'))
+    {
+       i = input.replace('...','${'+tokens[0].tokenName+'}');
+    }
+    return i;
 }
 
 const app = {
